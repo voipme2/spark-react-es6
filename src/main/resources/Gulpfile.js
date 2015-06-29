4e707c76f4987085f6656c57eedcaa9f0147346c
Patrick Grimard <pgrimard@gmail.com>
2015-06-29 11:40:38 -0400

    Separated Browserify output for vendor and app related scripts.

--- src/main/resources/Gulpfile.js
+++ src/main/resources/Gulpfile.js
@@ -1,27 +1,62 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
 var rename = require('gulp-rename');
 var gulpif = require('gulp-if');
 
+var browserify = require('browserify');
 var babelify = require('babelify');
 var del = require('del');
+var source = require('vinyl-source-stream');
+var resolve = require('resolve');
 
 var production = (process.env.NODE_ENV === 'production');
+
+gulp.task('default', ['clean', 'vendor', 'scripts'], function() {
+  gulp.src('./app/index.html')
+    .pipe(gulp.dest('public'));
+});
 
 gulp.task('clean', function(cb) {
   return del(['public'], cb);
 });
 
-gulp.task('default', ['clean', 'scripts'], function() {
-  gulp.src('./app/index.html')
-    .pipe(gulp.dest('public'));
+gulp.task('vendor', function() {
+  var b = browserify({debug: !production});
+
+  getNPMPackageIds().forEach(function(id) {
+    b.require(resolve.sync(id), {expose: id});
+  });
+
+  return b.bundle().pipe(source('vendor.js'))
+    .pipe(gulpif(production, streamify(uglify())))
+    .pipe(rename('vendor.min.js'))
+    .pipe(gulp.dest('./public/scripts'));
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('app/scripts/app.js', {read: false})
-    .pipe(browserify({debug: !production, transform: babelify}))
-    .pipe(uglify())
+  var b = browserify({debug: !production})
+    .require('./app/scripts/app.js', {entry: true})
+    .transform(babelify);
+
+  getNPMPackageIds().forEach(function(id) {
+    b.external(id);
+  });
+
+  return b.bundle().pipe(source('scripts.js'))
+    .pipe(gulpif(production, streamify(uglify())))
     .pipe(rename('scripts.min.js'))
-    .pipe(gulp.dest('public/scripts'));
+    .pipe(gulp.dest('./public/scripts'));
 });
+
+function getNPMPackageIds() {
+  // read package.json and get dependencies' package ids
+  var packageManifest = {};
+  try {
+    packageManifest = require('./package.json');
+  } catch (e) {
+    // does not have a package.json manifest
+  }
+  return Object.keys(packageManifest.dependencies) || [];
+
+}
